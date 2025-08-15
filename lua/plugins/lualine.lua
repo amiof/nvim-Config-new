8b8df936abd5e30408e9fd6547d5542ee3a80fa9
amiof <arm131313@gmail.com>
2025-08-15 20:25:36 +0330

    add lualine when open neo-tree

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -227,7 +227,8 @@ return {
         theme = "auto",
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
-        disabled_filetypes = { "alpha", "dashboard", "packer", "neo-tree" },
+        -- disabled_filetypes = { "alpha", "dashboard", "packer", "neo-tree" },
+        disabled_filetypes = { "alpha", "dashboard", "packer" },
         always_divide_middle = true,
       },
       sections = {
